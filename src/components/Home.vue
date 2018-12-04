d29ce7e3a5ee7f7fc378fe923fe21481200af29d
ffaiip <kawintip41@gmail.com>
2018-12-04 15:18:23 +0700

    Edited UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,11 +11,9 @@
                             <v-card-text>
                                 <div>Class</div>
                             </v-card-text>
-                            <v-combobox
-                                class="pl-3"
-                                v-model="classIP"
-                                :items="classes"
-                            ></v-combobox>
+                            <v-card-text>
+                                <h4>{{ this.classIP }}</h4>
+                            </v-card-text>
                         </v-flex>
                         <v-spacer></v-spacer>
                         <v-flex xs6>
@@ -29,6 +27,7 @@
                                         name="ip1"
                                         id="ip1"
                                         v-model="ip1"
+                                        maxlength="3"
                                         required>
                                     </v-text-field>
                                 </v-flex>
@@ -38,8 +37,7 @@
                                         name="ip2"
                                         id="ip2"
                                         v-model="ip2"
-                                        :label="defaultA"
-                                        :disabled="classA"
+                                        maxlength="3"
                                         required>
                                     </v-text-field>
                                 </v-flex>
@@ -49,8 +47,7 @@
                                         name="ip3"
                                         id="ip3"
                                         v-model="ip3"
-                                        :label="defaultB"
-                                        :disabled="classB"
+                                        maxlength="3"
                                         required>
                                     </v-text-field>
                                 </v-flex>
@@ -60,8 +57,7 @@
                                         name="ip4"
                                         id="ip4"
                                         v-model="ip4"
-                                        :label="defaultC"
-                                        :disabled="classC"
+                                        maxlength="3"
                                         required>
                                     </v-text-field>
                                 </v-flex>
@@ -105,6 +101,51 @@
                             </v-layout>
                         </v-flex>
                     </v-layout>
+                    <v-layout>
+                        <v-flex xs6>
+                            <v-card-text>
+                                <div>Range</div>
+                            </v-card-text>
+                            <v-layout row>
+                                <v-flex xs4>
+                                    <v-text-card>
+                                        {{ this.firstAdd }}
+                                    </v-text-card>
+                                </v-flex>
+                                <v-flex xs4>
+                                    <v-text-card>
+                                        {{ this.lastAdd }}
+                                    </v-text-card>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                        <v-flex>
+                            <v-card-text>
+                                <div>Subnet ID</div>
+                            </v-card-text>
+                            <v-layout row>
+                                <v-flex xs3>
+                                    <v-card-text>
+                                        <h4 class="headline mb-0">{{ this.subnetID}}</h4>
+                                    </v-card-text>
+                                </v-flex>    
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout>
+                        <v-flex>
+                            <v-card-text>
+                                <div>Broadcast</div>
+                            </v-card-text>
+                            <v-layout>
+                                <v-flex xs3>
+                                    <v-card-text>
+                                        <h4 class="headline mb-0">{{ this.broadcast }}</h4>
+                                    </v-card-text>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn
@@ -113,23 +154,6 @@
                         >Calculate</v-btn>
                     </v-card-actions>
                 </v-card>
-            </v-flex>
-        </v-layout>
-        <v-layout row>
-            <v-flex xs12>
-                <v-data-table
-                    :headers="headers"
-                    :items="subnets"
-                    class="elevation-1"
-                    >
-                    <template slot="items" slot-scope="props">
-                        <td>{{ props.item.number }}</td>
-                        <td class="text-xs-right">{{ props.item.subnetID }}</td>
-                        <td class="text-xs-right">{{ props.item.first }}</td>
-                        <td class="text-xs-right">{{ props.item.last }}</td>
-                        <td class="text-xs-right">{{ props.item.broadcast }}</td>
-                        </template>
-                </v-data-table>
             </v-flex>
         </v-layout>
     </v-container>
@@ -145,133 +169,31 @@ export default {
             ip4: '',
             amount: '',
             hosts: '',
-            classIP: '',
-            classes: [
-                "A", "B", "C"
-            ],
+            firstAdd: '',
+            lastAdd: '',
             hostsNet: [
                 "Hosts per Network", "Networks"
             ],
             subnetMask: '255.255.128.0',
-            headers: [{
-                            text: 'Subnet',
-                            align: 'left',
-                            value: 'number'
-                        },
-                        { text: 'Subnet ID', value: 'subnetID', sortable: false },
-                        { text: 'First add', value: 'first', sortable: false },
-                        { text: 'Last add', value: 'last', sortable: false },
-                        { text: 'Broadcast', value: 'broadcast', sortable: false },
-            ],
-            subnets: [{
-                        value: false,
-                        number: '0',
-                        subnetID: '159.36.24.13',
-                        first: '139.22.32.9',
-                        last: '13.49.224.4',
-                        broadcast: '13.42.124.255',
-                    },
-                    {
-            value: false,
-            number: '1',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '2',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '3',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '4',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '5',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '6',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '7',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '8',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          },
-          {
-            value: false,
-            number: '9',
-            subnetID: '159.36.24.13',
-            first: '139.22.32.9',
-            last: '13.49.224.4',
-            broadcast: '13.42.124.255',
-          }
-        ]
-      
+            broadcast: '',
+            subnetID: '',
         }
     },
     computed: {
-        defaultC () {
-            if (this.classIP == "C" || this.classIP == "B" || this.classIP == "A") {
-                return "0"
+        classIP () {
+            if (this.ip1 == ""){
+                return " "
             }
-        },
-        defaultB () {
-            if (this.classIP == "B" || this.classIP == "A") {
-                return "0"
+            else if(this.ip1 <= 127){
+                return "A"
             }
-        },
-        defaultA () {
-            if (this.classIP == "A") {
-                return "0"
+            else if(this.ip1 <= 191 && this.ip1 >= 128){
+                return "B"
             }
-        },
-        classC () {
-            return this.classIP == "C" || this.classIP == "B" || this.classIP == "A"
-        },
-        classB () {
-            return this.classIP == "B" || this.classIP == "A"
-        },
-        classA () {
-            return this.classIP == "A"
-        },
+            else if(this.ip1 >= 192 && this.ip1 <= 223){
+                return "C"
+            }
+        },  
         ip () {
             if (this.classIP == "C"){
                 return this.ip4 == '0'
